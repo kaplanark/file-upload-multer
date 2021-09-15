ac4a2e541e0739e92d1e60d1e774ad4eca69ba84
kaplanark <kaplan.arkan@gmail.com>
2021-09-15 14:16:54 +0300

    add mongodb

--- App.js
+++ App.js
@@ -3,11 +3,17 @@ import multer from "multer";
 import flash from "connect-flash";
 import session from "express-session";
 import path from "path";
-
-const upload = multer({ dest: "uploads/" });
 const app = express();
-const router = express.Router();
 const __dirname = path.resolve();
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+var upload = multer({ storage: storage });
 
 app.use(
   session({
@@ -24,6 +30,7 @@ app.use(function (req, res, next) {
   res.locals.errors = req.flash("error");
   res.locals.message = req.flash("message");
   res.locals.info = req.flash("info");
+  res.locals.warning = req.flash("warning");
   next();
 });
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
@@ -34,42 +41,35 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/upload", upload.single("photo"), function(req, res, next){
-  if(req.file){
+app.post("/upload", upload.single("file"), function (req, res, next) {
+  if (!req.file) {
+    req.flash("warning", "choose a file");
+    res.redirect("/");
+  } else {
     req.flash("message", `${req.file.originalname}`);
     res.redirect("/");
-  }else{
-    req.flash("message", "choose a file");
-    res.redirect("/");
   }
 });
 
-app.post("/uploads", upload.array("photos", 12), (req, res, next) => {
-  if(req.fields){
+app.post("/uploads", upload.array("files", 12), (req, res, next) => {
+  if (!req.fields) {
+    req.flash("warning", "choose a file");
+    res.redirect("/");
+  } else {
     req.flash("message", `${req.fields}`);
     res.redirect("/");
-  }else{
-    req.flash("message", "choose a file");
-    res.redirect("/");
   }
 });
 
-app.post(
-  "/files",
-  upload.fields([
-    { name: "video", maxCount: 2 },
-    { name: "photo", maxCount: 2 },
-  ]),
-  (req, res, next) => {
-    if(req.file){
-      req.flash("message", `${req.file.originalname}`);
-      res.redirect("/");
-    }else{
-      req.flash("message", "choose a file");
-      res.redirect("/");
-    }
+app.post("/uploadphoto", upload.single("photo"), function (req, res, next) {
+  if (!req.file) {
+    req.flash("warning", "choose a file");
+    res.redirect("/");
+  } else {
+    req.flash("message", `${req.file.originalname}`);
+    res.redirect("/");
   }
-);
+});
 
 app.listen(5000, function (err) {
   if (err) {
